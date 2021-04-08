aee943d2ca98cb1eadb95476f51d155807fc2599
Eduardo Bravo <eduardobravopires@gmail.com>
2021-04-08 18:14:47 -0300

    finished header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -71,7 +71,7 @@ export function Header() {
         <Flex align="center">
           <Box mr="4" textAlign="right">
             <Text>Eduardo Bravo</Text>
-            <Text color="gray.300" fontSize="small">eduardo@gmail.com</Text>
+            <Text color="gray.300" fontSize="sm">eduardo@gmail.com</Text>
           </Box>
 
           <Avatar size="md" name="Eduardo Bravo" src="https://github.com/eduardobravop.png" />
